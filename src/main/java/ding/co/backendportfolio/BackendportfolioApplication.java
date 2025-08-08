32ac6b19de4ba867e9de3a5dcdd5536708abab71
yeonjookang <96986782+yeonjookang@users.noreply.github.com>
2025-08-08 17:12:03 +0900

    feat: 나의 각오 문장 출력

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
+        System.out.println("젊을 때 고생은 사서도 한다!");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
